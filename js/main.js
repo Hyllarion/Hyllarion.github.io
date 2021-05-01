07099744127c5e68a0bd2ac845389755d8788464
Dominic Pietz <dominic.pietz@ipm.fraunhofer.de>
2021-05-01 15:04:23 +0200

    neues Rätsel - Badenrätsel

--- js/main.js
+++ js/main.js
@@ -1,45 +1,101 @@
-var audio_SA = new Audio("./data/audio/Sprung-Auf.mp3");
-var audio_falsch = new Audio("./data/audio/falsch.m4a");
+var descriptions = [
+    "Schutz, Obdach",
+    "Schöpfkelle, Schimpfwort für Frauen",
+    "Sieb, Filter, Trichter",
+    "Tüte",
+    "Schnuller",
+    "moderig, muffig riechen",
+    "schlechtes Messer oder Gerät",
+    "mittelgroßer Korb",
+    "Nachttopf",
+    "Handfeger",
+    "fahrendes Volk, Lausbub, unstete Menschen",
+    "Nichtsnutz",
+    "mürrischer Mensch",
+    "Schimpfwort für Frau",
+    "einfältiger ungeschickter Mann",
+    "unartiges Kind, Göre",
+    "begriffsstutzige liederliche Person",
+    "Musikant, aufschneiderische männliche Person",
+    "hagere geschwätzige Person",
+    "fiktive Person"
+];
 
-function check1() {
-	var sum = "";
-	var color = "";
-	for (var i = 0; i < 3; i++) {
-		sum += document.getElementById("raetsel1").getElementsByTagName("input")[i].value;
-	}
-	if (sum == "264") {
-		document.getElementById("raetsel1").getElementsByTagName("output")[0].style.color = "green";
-		document.getElementById("raetsel1").getElementsByTagName("output")[0].innerHTML = "Rätsel richtig gelöst!";
-		color = "green";
-		play_audio_SA();
-		document.getElementById("raetsel1zusatz").style.display = "block";
-	} else {
-		document.getElementById("raetsel1").getElementsByTagName("output")[0].style.color = "red";
-		document.getElementById("raetsel1").getElementsByTagName("output")[0].innerHTML = "Leider falsch";
-		color = "red";
-		audio_falsch.play();
-		document.getElementById("raetsel1zusatz").style.display = "none";
-	}
-	for (var i = 0; i < 3; i++) {
-		sum += document.getElementById("raetsel1").getElementsByTagName("input")[i].style.backgroundColor = color;
-	}
+var words = [
+    "Schärmi",
+    "Schapfe",
+    "Seechter",
+    "Guggele",
+    "Lulli",
+    "michdele",
+    "Groddegiggser",
+    "Zeine",
+    "Boddschamber",
+    "Kehrwisch",
+    "Schiirebirzler",
+    "Lumbeseggel",
+    "Bruddli",
+    "Duendle",
+    "Dilldapp",
+    "Gof",
+    "Schelle",
+    "Schnitzgieger",
+    "Raffle",
+    "Bloßarsch"
+];
+
+var shuffleArray = [];
+
+function init() {
+
+    for (let i = 0; i < words.length; i++) {
+        while (true) {
+            let rdm = Math.floor(Math.random() * words.length);
+            if (!shuffleArray.includes(rdm)) {
+                shuffleArray.push(rdm);
+                break; // Only out of while
+            }
+        }
+    }
+
+    console.log(shuffleArray);
+
+    for (let i = 0; i < words.length; i++) {
+        var option = document.createElement('option');
+        option.innerHTML = "---";
+        document.getElementsByClassName('objectDescriptions')[i].appendChild(option);
+    }
+
+    var elements = document.getElementsByClassName('objectWords');
+    for (let i = 0; i < words.length; i++) {
+        elements[i].innerHTML = words[shuffleArray[i]];
+        var selections = document.getElementsByClassName('objectDescriptions');
+        for (let select of selections) {
+            var option = document.createElement('option');
+            option.innerHTML = descriptions[i];
+            select.appendChild(option);
+        }
+    }
 }
 
-function play_audio_SA() {
-	audio_SA.play();
-}
-
-function play_audio_falsch() {
-	audio_falsch.play();
-}
-
-function check1add() {
-	var name = document.getElementById("raetsel1zusatz").getElementsByTagName("input")[0].value.toLowerCase();
-	if (name == "konny" || name == "konni" || name == "kony" || name == "koni") {
-		document.getElementById("raetsel1zusatz").getElementsByTagName("output")[0].innerHTML = "Richtig!";
-		document.getElementById("raetsel1zusatz").getElementsByTagName("input")[0].style.backgroundColor = "green";
-	} else {
-		document.getElementById("raetsel1zusatz").getElementsByTagName("output")[0].innerHTML = "";
-		document.getElementById("raetsel1zusatz").getElementsByTagName("input")[0].style.backgroundColor = "white";
-	}
+function onClickCheckObjects() {
+    var select = document.getElementsByClassName('objectDescriptions');
+    let checkSum = 0;
+    for (let i = 0; i < words.length; i++) {
+        // console.log(select[i]['selectedIndex']);
+        // console.log(select[i].options[select[i]['selectedIndex']].text);
+        // console.log(descriptions[shuffleArray[i]]);
+        // console.log(descriptions[shuffleArray[i]] == select[i].options[select[i]['selectedIndex']].text);
+        if (descriptions[shuffleArray[i]] == select[i].options[select[i]['selectedIndex']].text) {
+            checkSum++;
+            document.getElementsByClassName("objectWords")[i].style.color = "green";
+        } else {
+            document.getElementsByClassName("objectWords")[i].style.color = "red";
+        }
+    }
+    if (checkSum == words.length) {
+        alert("Super,\nalles richtig!");
+    } else {
+        alert("Noch " + (words.length - checkSum) + " Fehler");
+    }
 }
