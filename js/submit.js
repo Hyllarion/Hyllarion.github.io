0321b1c4853aa9a8f180b93c4cf5e3ddef16f954
Dominic Pietz <dominicpietz@gmx.de>
2025-03-11 01:31:05 +0100

    make it beautiful, part 1

---
+++ js/submit.js
@@ -0,0 +1,10 @@
+// window.onload = () => {
+//     const btn = document.querySelector("#btn");
+//     const btnText = document.querySelector("#btnText");
+
+//     btn.onclick = (event) => {
+//         event.preventDefault();
+//         btnText.innerHTML = "Danke";
+//         btn.classList.add("active");
+//     };
+// }
