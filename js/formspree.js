56a5c12c57ca4344b14ae57b54969b178d51c974
Dominic Pietz <dominicpietz@gmx.de>
2025-03-23 13:48:23 +0100

    improve ignoring fields that are not used but spam the mail

--- js/formspree.js
+++ js/formspree.js
@@ -2,6 +2,10 @@ function changeNumGuests() {
     let numGuests = $("#numGuests").val();
     if (numGuests == 0) {
         $("#textAbsage").attr("name", "AbsageName");
+        $("#textAnmerkungen").removeAttr("name");
+        $("#textBeitrag").removeAttr("name");
+        $("#duration").removeAttr("name");
+        $("#textMitbringsel").removeAttr("name");
         document.getElementById("numGuests").style.width = "25rem";
         document.getElementById("absage-name").style.display = "block";
         document.getElementById("anmerkungen").style.display = "none";
@@ -13,6 +17,10 @@ function changeNumGuests() {
         document.getElementById("mitbringsel-trenner").style.display = "none";
     } else {
         $("#textAbsage").removeAttr("name");
+        $("#textAnmerkungen").attr("name", "Anmerkungen");
+        $("#textBeitrag").attr("name", "Beitrag");
+        $("#duration").attr("name", "Dauer-Beitrag");
+        $("#textMitbringsel").attr("name", "Mitbringsel");
         document.getElementById("numGuests").style.width = "10rem";
         document.getElementById("absage-name").style.display = "none";
         document.getElementById("anmerkungen").style.display = "block";
@@ -31,12 +39,14 @@ function changeNumGuests() {
             $("#radio-1-" + i).attr("name", "Essen-" + i);
             $("#radio-2-" + i).attr("name", "Essen-" + i);
             $("#radio-3-" + i).attr("name", "Essen-" + i);
+            $("#kind" + i).attr("name", "Kind" + i);
         } else {
             $("#guest" + i).addClass("hiddenGuest");
             $("#name" + i).removeAttr("name");
             $("#radio-1-" + i).removeAttr("name");
             $("#radio-2-" + i).removeAttr("name");
             $("#radio-3-" + i).removeAttr("name");
+            $("#kind" + i).removeAttr("name");
         }
     }
 }
